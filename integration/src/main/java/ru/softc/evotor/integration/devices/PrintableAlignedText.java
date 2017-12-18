488cb2b9202897d20ec6fcec6665387ffc0eff78
Alexander Soloshenko <capcom@soft-c.ru>
2017-12-18 09:52:54 +0700

    add aligned printable text

---
+++ integration/src/main/java/ru/softc/evotor/integration/devices/PrintableAlignedText.java
@@ -0,0 +1,53 @@
+package ru.softc.evotor.integration.devices;
+
+import android.os.Parcel;
+
+import ru.evotor.devices.commons.printer.printable.IPrintable;
+import ru.evotor.devices.commons.printer.printable.PrintableText;
+
+/**
+ * Created by capcom on 15.12.2017.
+ */
+
+public class PrintableAlignedText extends PrintableText {
+    public enum Align {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    private final Align align;
+
+    protected PrintableAlignedText(Parcel in) {
+        super(in.readString());
+        align = Align.values()[in.readInt()];
+    }
+
+    public PrintableAlignedText(Align align, String text) {
+        super(text);
+
+        this.align = align;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeInt(align.ordinal());
+    }
+
+    public static final Creator<PrintableAlignedText> CREATOR = new Creator<PrintableAlignedText>() {
+
+        public PrintableAlignedText createFromParcel(Parcel in) {
+            return new PrintableAlignedText(in);
+        }
+
+        public PrintableAlignedText[] newArray(int size) {
+            return new PrintableAlignedText[size];
+        }
+    };
+}
